201939792bc5a5e9123cfa6a9cadab30f08739f2
kkssry <kkssry@naver.com>
2019-08-17 00:03:34 +0900

    refactor - modify interface name

--- src/main/java/java_study/modern_java/CreateFunctionalInterfaceTest.java
+++ src/main/java/java_study/modern_java/CreateFunctionalInterfaceTest.java
@@ -5,13 +5,13 @@ public class CreateFunctionalInterfaceTest {
         println(1L, "Kevin", "test@email.com", (id, name, email) -> "User info: ID= " + id + ", Name= " + name + ", email= " + email);
     }
 
-    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function<T1, T2, T3, String> function) {
+    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
         System.out.println(function.apply(t1, t2, t3));
     }
 }
 
 @FunctionalInterface
-interface Function<T1, T2, T3, R> {
+interface Function3<T1, T2, T3, R> {
     R apply(T1 t1, T2 t2, T3 t3);
 }
 
