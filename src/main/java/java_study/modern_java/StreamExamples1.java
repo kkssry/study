05d2907e94e798476ddb3661ace49b000d049102
kkssry <kkssry@naver.com>
2019-08-19 02:16:52 +0900

    stream practice - loop

--- src/main/java/java_study/modern_java/StreamExamples1.java
+++ src/main/java/java_study/modern_java/StreamExamples1.java
@@ -1,4 +1,13 @@
 package java_study.modern_java;
 
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class StreamExamples1 {
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 10).forEach(i -> System.out.println(i + " "));     // include endIndex
+        IntStream.iterate(1, i -> i + 1).forEach(i-> System.out.print(i+" "));  // infinite increase
+        Stream.iterate(BigInteger.ONE , i-> i.add(BigInteger.ONE)).forEach(i-> System.out.print(i + " ")); // infinite increase
+    }
 }
