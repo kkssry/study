c2b9c93024501cb08754dd022a05404621634c42
kkssry <kkssry@naver.com>
2019-08-19 11:51:27 +0900

    old way vs functional interface way

---
+++ src/main/java/java_study/modern_java/StreamExamples2.java
@@ -0,0 +1,32 @@
+package java_study.modern_java;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExamples2 {
+    public static void main(String[] args) {
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Integer result = null;
+        for (Integer number : numbers) {
+            if (number > 3 && number < 9) {
+                final Integer newNumber = number * 2;
+                if (newNumber > 10) {
+                    result = newNumber;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Imperative result: " + result);
+
+        System.out.println("Functional Result: " +
+                numbers.stream()
+                        .filter(number -> number > 3)
+                        .filter(number -> number < 9)
+                        .map(number -> number * 2)
+                        .filter(number -> number > 10)
+                        .findFirst()
+        );
+
+    }
+}
